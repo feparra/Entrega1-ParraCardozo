15cb43eed99160a9b7ef2622c6b6a0c79790f3e8
feparra <gerenciaemarketing@gmail.com>
2022-06-27 09:05:56 -0400

    subiendo dashboard

--- tradingbookapp/urls.py
+++ tradingbookapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views #se utiliza el punto por que es la ruta relativa (donde esta la carpeta)
 
 urlpatterns = [
-    path('',views.index, name='index'),
+    path('home/',views.index, name='index'),
     path('dashboard/',views.Dashboard,name="dashboard"),
     path('notes/',views.Notes,name="notes"),
     path('trades/',views.Trades,name="trades"),
