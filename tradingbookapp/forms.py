c741c925fc51d3c41bb8b32017fd07762ba37545
feparra <gerenciaemarketing@gmail.com>
2022-06-28 20:16:35 -0400

    creando casillas de busqueda

--- tradingbookapp/forms.py
+++ tradingbookapp/forms.py
@@ -1,6 +1,25 @@
 from django import forms
+from django.forms.widgets import NumberInput
 
-class NuevoTrade(forms.Form):
-    simbolo = forms.CharField()
-    entrada = forms.IntegerField()
+posiciones=(
+    ("1","Long"),
+    ("2","Short"),
     
+)
+class NuevoTrade(forms.Form):
+    fecha = forms.DateTimeField(label="fecha ", required=True, widget=NumberInput(attrs={'type':'date'}))
+    simbolo = forms.CharField(label="simbolo")
+    posicion = forms.ChoiceField(label="posicion",choices=posiciones)
+    entrada = forms.FloatField(label="Precio Entrada",)
+    target = forms.FloatField(label="Target")
+    stop = forms.FloatField(label="stop")
+
+class NuevaTradingnote(forms.Form):
+    fecha = forms.DateTimeField(label="fecha ", required=True, widget=NumberInput(attrs={'type':'date'}))
+    simbolo = forms.CharField(label="simbolo")
+    nota = forms.CharField(label="Nota",widget=forms.Textarea(attrs={"rows":5, "cols":30}))
+
+
+class NuevoMercado(forms.Form):
+    pais =forms.CharField(label="pais")
+    simbolo = forms.CharField(label="simbolo")
--- tradingbookapp/views.py
+++ tradingbookapp/views.py
@@ -1,6 +1,7 @@
+
 from django.shortcuts import render,redirect
 from .models import Trade,Note,Market
-from .forms import NuevoTrade
+from .forms import NuevoTrade,NuevaTradingnote,NuevoMercado
 
 def index(request):
     return render (request,'tradingbookapp/index.html')
@@ -10,10 +11,25 @@ def Challenge(request):
     return render (request,'tradingbookapp/challenge.html')
 
 
+def buscar_trade(request):
+    tradesexistente = []
+    return render(request,'tradingbookapp/buscar_trade.html',{"trades":tradesexistente})
+
+def buscar_note(request):
+    notesexistente = []
+    return render(request,'tradingbookapp/buscar_note.html',{"trades":notesexistente})
+
+def buscar_mercado(request):
+    mercadosexistente = []
+    return render(request,'tradingbookapp/buscar_mercado.html',{"trades":mercadosexistente})
+
+
+
+
 def Trades(request):
     trades = Trade.objects.all()
     ctx = {'trades':trades}
-    return render(request,'tradingbookapp/Trades.html',ctx)
+    return render(request,'tradingbookapp/trades.html',ctx)
 
 def Markets(request):
     markets = Market.objects.all()
@@ -26,12 +42,21 @@ def base(request):
 
 
 
-def crear_trade(request): 
-    #post
+def crear_trade(request):
+    
     if request.method =="POST":
-        trade = Trade(fecha = request.POST['fecha'],simbolo = request.POST['simbolo'],posicion = request.POST['posicion'],entrada = request.POST['entrada'],target = request.POST['target'], stop = request.POST['stop'])
-        trade.save()
-        return redirect("Home")
+        #post
+        formulario = NuevoTrade(request.POST)
+        if formulario.is_valid():
+            
+            info_trade = formulario.cleaned_data
+            
+            trade = Trade(fecha = request.POST['fecha'],simbolo = request.POST['simbolo'],posicion = request.POST['posicion'],entrada = request.POST['entrada'],target = request.POST['target'], stop = request.POST['stop'])
+            trade.save()
+            return redirect("Home")
+        
+        else:
+            return render(request,'tradingbookapp/formulario_trade.html',{"form":formulario})
         
     else: #get y otros:
         
@@ -44,21 +69,36 @@ def crear_trade(request):
 def crear_notes(request):
     
     if request.method =="POST":
-        note = Note(fecha = request.POST['fecha'],simbolo = request.POST['simbolo'],nota = request.POST['nota'])
-        note.save()
-        return redirect("Home")
+        #post
+        formulario = NuevaTradingnote(request.POST)
+        if formulario.is_valid():
+            
+            info_note = formulario.cleaned_data
+            note = Note(fecha = request.POST['fecha'],simbolo = request.POST['simbolo'],nota = request.POST['nota'])
+            note.save()
+            return redirect("Home")
+        else:
+            return render(request,'tradingbookapp/formulario_notes.html',{"form":formulario})
         
     else:
-        return render(request,'tradingbookapp/formulario_notes.html')
+        notavacia = NuevaTradingnote()
+        return render(request,'tradingbookapp/formulario_notes.html',{"form":notavacia})
 
 def crear_mercados(request):
     if request.method =="POST":
-        mercado = Market(pais= request.POST['pais'],simbolo= request.POST['simbolo'])
-        mercado.save()
-        return redirect("Home")
-        
-    else:    
-        return render(request,'tradingbookapp/formulario_mercados.html')
+        #post
+        formulario = NuevoMercado(request.POST)
+        if formulario.is_valid():
+
+            mercado = Market(pais= request.POST['pais'],simbolo= request.POST['simbolo'])
+            mercado.save()
+            return redirect("Home")
+        else:
+            return render(request,'tradingbookapp/formulario_mercados.html',{"form":formulario})
+            
+    else:  
+        mercadovacio = NuevoMercado()  
+        return render(request,'tradingbookapp/formulario_mercados.html',{"form":mercadovacio})
 
 
 def Dashboard(request):
